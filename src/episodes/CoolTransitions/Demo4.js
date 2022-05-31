1bc761f16e5174b6e4805535b98439f58809e40c
Mateusz Krystkowicz <krissie515@gmail.com>
2022-05-31 14:40:13 +0200

    add svg animation

--- src/episodes/CoolTransitions/Demo4.js
+++ src/episodes/CoolTransitions/Demo4.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-import gsap from 'gsap';
-import image from './discover.jpg';
+import gsap from "gsap";
+import image from "./discover.jpg";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -18,13 +18,13 @@ const StyledHalfBox = styled.div`
   align-items: center;
   width: 50vw;
   height: 100vh;
-  flex-direction: ${({isColumn}) => isColumn ? 'column' : 'row'};
+  flex-direction: ${({ isColumn }) => (isColumn ? "column" : "row")};
 `;
 
 const StyledText = styled.span`
   font-size: 105px;
   font-weight: bold;
-  color: ${({isWhite}) => isWhite ? 'white' : 'black'};
+  color: ${({ isWhite }) => (isWhite ? "white" : "black")};
 `;
 
 const StyledContent = styled(StyledHalfBox)`
@@ -52,52 +52,57 @@ const StyledParagraph = styled.p`
 `;
 
 const Demo4 = () => {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const tl = React.useRef(null);
-    const curtainRef = React.useRef(null);
-    const titleRef = React.useRef(null);
-    const contentRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const tl = React.useRef(null);
+  const curtainRef = React.useRef(null);
+  const titleRef = React.useRef(null);
+  const contentRef = React.useRef(null);
 
-    React.useEffect(() => {
-        tl.current = gsap.timeline({paused: true});
+  React.useEffect(() => {
+    tl.current = gsap.timeline({ paused: true });
 
-        tl.current
-            .addLabel('start')
-            .to([curtainRef.current.children, titleRef.current, contentRef.current], {
-                x: '-50vw',
-                duration: 0.5,
-                easing: 'cubic-bezier(.47,.93,0,1.02)',
-                stagger: 0.03
-            }, 'start')
-    }, [])
+    tl.current.addLabel("start").to(
+      [curtainRef.current.children, titleRef.current, contentRef.current],
+      {
+        x: "-50vw",
+        duration: 0.5,
+        easing: "cubic-bezier(.47,.93,0,1.02)",
+        stagger: 0.03,
+      },
+      "start"
+    );
+  }, []);
 
-    const handleClick = () => {
-        if (isOpen) {
-            tl.current.reverse();
-            setIsOpen(!isOpen);
-        } else {
-            tl.current.play();
-            setIsOpen(!isOpen);
-        }
+  const handleClick = () => {
+    if (isOpen) {
+      tl.current.reverse();
+      setIsOpen(!isOpen);
+    } else {
+      tl.current.play();
+      setIsOpen(!isOpen);
     }
+  };
 
-    return (
-        <Wrapper>
-            <StyledHalfBox ref={titleRef}>
-                <StyledText>Discover</StyledText>
-            </StyledHalfBox>
-            <StyledCurtain ref={curtainRef} onClick={handleClick}>
-                <img src={image} alt=""/>
-                <StyledText isWhite>More</StyledText>
-            </StyledCurtain>
-            <StyledContent isColumn ref={contentRef}>
-                <StyledParagraph>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci asperiores atque blanditiis
-                    commodi consequuntur ducimus ea earum in ipsum iste itaque iusto minus natus necessitatibus numquam,
-                    quam quos recusandae rerum!</StyledParagraph>
-                <StyledParagraph>[click here to close]</StyledParagraph>
-            </StyledContent>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <StyledHalfBox ref={titleRef}>
+        <StyledText>Discover</StyledText>
+      </StyledHalfBox>
+      <StyledCurtain ref={curtainRef} onClick={handleClick}>
+        <img src={image} alt="" />
+        <StyledText isWhite>More</StyledText>
+      </StyledCurtain>
+      <StyledContent isColumn ref={contentRef}>
+        <StyledParagraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+          asperiores atque blanditiis commodi consequuntur ducimus ea earum in
+          ipsum iste itaque iusto minus natus necessitatibus numquam, quam quos
+          recusandae rerum!
+        </StyledParagraph>
+        <StyledParagraph>[click here to close]</StyledParagraph>
+      </StyledContent>
+    </Wrapper>
+  );
 };
 
 export default Demo4;
