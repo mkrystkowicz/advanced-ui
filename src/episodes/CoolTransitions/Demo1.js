0b8c9d30403eeb20e591cdba469b0c727c113fa9
Mateusz Krystkowicz <krissie515@gmail.com>
2022-05-31 12:16:07 +0200

    add demo 1

--- src/episodes/CoolTransitions/Demo1.js
+++ src/episodes/CoolTransitions/Demo1.js
@@ -1,6 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import {ButtonB} from "../FancyButtons/FancyButtons";
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { ButtonB } from "../FancyButtons/FancyButtons";
+
+const MoveUpStart = keyframes`
+  to {
+    transform: translateY(-105%);
+  }
+`;
+
+const MoveUpEnd = keyframes`
+  from {
+    transform: translateY(100%);
+  } to {
+    transform: translateY(0);
+  }
+`;
+
+const SlideLeft = keyframes`
+  0% {
+    transform: translate(100%);
+  }
+  25% {
+    transform: translateX(0);
+  }
+  75% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translate(-100%);
+  }
+`;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -10,7 +39,7 @@ const Wrapper = styled.div`
   align-items: center;
 
   &::before {
-    content: '';
+    content: "";
     position: absolute;
     top: 0;
     left: 0;
@@ -19,11 +48,11 @@ const Wrapper = styled.div`
     height: 100%;
     background-color: #181818;
     transform-origin: 0 0;
+    animation: ${SlideLeft} 1s 0.5s forwards;
   }
 `;
 
 const TextContainer = styled.div`
-  overflow: hidden;
   transform: rotate(-25deg);
 `;
 
@@ -37,7 +66,8 @@ const StyledText = styled.span`
 
   &::before {
     display: block;
-    content: '${({content}) => content ? content : ''}';
+    content: "${({ content }) => (content ? content : "")}";
+    animation: ${MoveUpStart} 0.5s forwards, ${MoveUpEnd} 0.5s 1.5s forwards;
   }
 `;
 
@@ -49,16 +79,36 @@ const StyledButton = styled(ButtonB)`
   top: 70%;
 `;
 
+const firstSlide = ["Start", "here"];
+const lastSlide = ["End", "there"];
+
 const Demo1 = () => {
-    return (
-        <Wrapper>
-            <TextContainer>
-                <StyledText content="Start" />
-                <StyledText content="here" />
-            </TextContainer>
-            <StyledButton>Next</StyledButton>
-        </Wrapper>
-    )
+  const [toggle, setToggle] = React.useState(firstSlide);
+  const [content, setContent] = React.useState(firstSlide);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      if (toggle) {
+        setContent(firstSlide);
+      } else {
+        setContent(lastSlide);
+      }
+    }, 600);
+  }, [toggle]);
+
+  return (
+    <Wrapper key={toggle ? 1 : 2}>
+      <TextContainer>
+        <StyledText content={content[0]} />
+        <StyledText content={content[1]} />
+      </TextContainer>
+      <StyledButton onClick={handleClick}>Next</StyledButton>
+    </Wrapper>
+  );
 };
 
 export default Demo1;
